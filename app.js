6edb0b2210906f0967fed8dfd79cbf6f12886cc8
Dileepar26 <dileep.ram26@gmail.com>
2024-12-22 11:39:14 +0530

    front page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const authRoutes = require('./src/routes/authRoutes');
 // --------------------------------------------------------------------------------------------------
 app.use(express.json()); // parse json
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 // --------------------------------------------------------------------------------------------------
 app.use(function (req, res, next) {
     const requestIncomingTime = Date.now();
@@ -21,10 +22,15 @@ app.use(function (req, res, next) {
 
 app.use('/auth', authRoutes);
 app.use('/api', urlRoutes);
+
 app.use('*', (req, res) => {
     res.status(404).send('<h1>page not found</h1>')
-    // res.status(404).json({data: 1})
 })
+
+// Serve default page for /
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 const HTTP_PORT = 3000
 app.listen(HTTP_PORT, function () {
     logger.info(`HTTP Server started at port ${HTTP_PORT}`);
